5fd3eb158b197d522020406c2b12d7f4bd9d4187
Aditya Hota <adihota@gmail.com>
2021-03-01 22:44:04 -0500

    Update capture to write to separate output directory

--- ImageCapture/capture.py
+++ ImageCapture/capture.py
@@ -1,4 +1,10 @@
 import cv2
+import os
+
+output_dir = "output_images"
+if not os.path.exists(output_dir):
+    os.mkdir(output_dir)
+
 vc = cv2.VideoCapture(0)
 if vc.isOpened():
     rval, frame = vc.read() 
@@ -12,6 +18,9 @@ while rval:
     if key == 27: # exit on ESC 
         break
     elif key == 32:
-        cv2.imwrite("./output_images/test.png", frame);
+        print("Writing image...")
+        output_filename = "test.png"
+        cv2.imwrite(os.path.join(output_dir, output_filename), frame);
+        print(f"Image '{output_filename}' written.\n")
 
 cv2.destroyWindow("Image Preview")
